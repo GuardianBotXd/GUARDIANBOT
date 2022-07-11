1f095d155dddf828849724aa8ed29ed327e9fcd8
DinoGuardianXD <108822100+DinoGuardianXD@users.noreply.github.com>
2022-07-11 13:22:47 +0530

    GuardianString.py

--- LegendString.py
+++ LegendString.py
@@ -15,7 +15,7 @@ if okvai == "69":
         client.send_message("me", client.session.save())
         client.send_message(
             "me",
-            "Above is your #LEGENDBOT STRING SESSION \nPaste this string in Heroku Var.\n\n[Team LegendBot](t.me/LegendBot_AI)",
+            "Above is your #GUARDIANBOT STRING SESSION \nPaste this string in Heroku Var.\n\n[Team GUARDIANBot](t.me/GuardianBot_AI)",
         )
 
 else:
