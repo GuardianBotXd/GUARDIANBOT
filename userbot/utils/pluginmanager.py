79b2599719e79de789cd665438332b5e013ef239
DinoGuardianXD <108822100+DinoGuardianXD@users.noreply.github.com>
2022-07-14 11:15:07 +0530

    Update pluginmanager.py

--- userbot/utils/pluginmanager.py
+++ userbot/utils/pluginmanager.py
@@ -8,12 +8,12 @@ from ..Config import Config
 from ..core import LOADED_CMDS, PLG_INFO
 from ..core.logger import logging
 from ..core.managers import eod, eor
-from ..core.session import legend
+from ..core.session import guardian
 from ..helpers.tools import media_type
-from ..helpers.utils import _format, _legendtools, _legendutils, install_pip, reply_id
+from ..helpers.utils import _format, _guardiantools, _guardianutils, install_pip, reply_id
 from .decorators import admin_cmd, sudo_cmd
 
-LOGS = logging.getLogger("LegendUserBot")
+LOGS = logging.getLogger("GuardianUserBot")
 
 
 def load_module(shortname, plugin_path=None):
@@ -41,13 +41,13 @@ def load_module(shortname, plugin_path=None):
         mod.LOGS = LOGS
         mod.Config = Config
         mod._format = _format
-        mod.tgbot = legend.tgbot
+        mod.tgbot = guardian.tgbot
         mod.sudo_cmd = sudo_cmd
         mod.CMD_HELP = CMD_HELP
         mod.reply_id = reply_id
         mod.admin_cmd = admin_cmd
-        mod._legendutils = _legendutils
-        mod._legendtools = _legendtools
+        mod._guardianutils = _guardianutils
+        mod._guardiantools = _guardiantools
         mod.media_type = media_type
         mod.eod = eod
         mod.install_pip = install_pip
@@ -58,7 +58,7 @@ def load_module(shortname, plugin_path=None):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        LOGS.info("LegendBot " + shortname)
+        LOGS.info("GuardianBot " + shortname)
 
 
 def start_spam(shortname):
@@ -85,7 +85,7 @@ def start_spam(shortname):
         name = "userbot.plugins.Spam.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        mod.tgbot = legend.tgbot
+        mod.tgbot = guardian.tgbot
         spec.loader.exec_module(mod)
         sys.modules["Spam" + shortname] = mod
         print("[🔰Spam🔰 3.0] ~ HAS ~ 💞Installed💞 ~" + shortname)
@@ -101,23 +101,23 @@ def remove_plugin(shortname):
         for cmdname in cmd:
             if cmdname in LOADED_CMDS:
                 for i in LOADED_CMDS[cmdname]:
-                    legend.remove_event_handler(i)
+                    guardian.remove_event_handler(i)
                 del LOADED_CMDS[cmdname]
         return True
     except Exception as e:
         LOGS.error(e)
     try:
         for i in LOAD_PLUG[shortname]:
-            legend.remove_event_handler(i)
+            guardian.remove_event_handler(i)
         del LOAD_PLUG[shortname]
     except BaseException:
         pass
     try:
         name = f"userbot.plugins.{shortname}"
-        for i in reversed(range(len(legend._event_builders))):
-            ev, cb = legend._event_builders[i]
+        for i in reversed(range(len(guardian._event_builders))):
+            ev, cb = guardian._event_builders[i]
             if cb.__module__ == name:
-                del legend._event_builders[i]
+                del guardian._event_builders[i]
     except BaseException:
         raise ValueError
 
