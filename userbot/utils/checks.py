43271914022a500c0c44d9a34bd30c46748ab035
DinoGuardianXD <108822100+DinoGuardianXD@users.noreply.github.com>
2022-07-14 11:43:37 +0530

    Update checks.py

--- userbot/utils/checks.py
+++ userbot/utils/checks.py
@@ -6,11 +6,11 @@ LOGS = logging.getLogger(__name__)
 
 
 # Admin checker by uniborg
-async def is_admin(legend, chat_id, userid):
+async def is_admin(guardian, chat_id, userid):
     if not str(chat_id).startswith("-100"):
         return False
     try:
-        req_jo = await legend.get_permissions(chat_id, userid)
+        req_jo = await guardian.get_permissions(chat_id, userid)
         chat_participant = req_jo.participant
         if isinstance(
             chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
