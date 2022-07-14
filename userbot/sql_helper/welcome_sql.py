5e19007f1d645f6eb848e5f4d8276befa7d7f984
DinoGuardianXD <108822100+DinoGuardianXD@users.noreply.github.com>
2022-07-14 11:47:51 +0530

    Update welcome_sql.py

--- userbot/sql_helper/welcome_sql.py
+++ userbot/sql_helper/welcome_sql.py
@@ -7,7 +7,7 @@ from sqlalchemy import BigInteger, Column, Numeric, String, UnicodeText
 
 
 class Welcome(BASE):
-    __tablename__ = "legendwelcome"
+    __tablename__ = "guardianwelcome"
     chat_id = Column(String(14), primary_key=True)
     previous_welcome = Column(BigInteger)
     reply = Column(UnicodeText)
