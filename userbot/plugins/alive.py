ff12b82a717e5efec39fc69628a0c1f01451324a
DinoGuardianXD <108822100+DinoGuardianXD@users.noreply.github.com>
2022-07-16 22:56:58 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,11 +13,11 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from userbot import StartTime, legend, legendversion
+from userbot import StartTime, guardian, guardianversion
 
 from ..Config import Config
 from ..core.managers import eor
-from ..helpers.functions import check_data_base_heal_th, get_readable_time, legendalive
+from ..helpers.functions import check_data_base_heal_th, get_readable_time, guardianalive
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import mention
@@ -25,14 +25,14 @@ from . import mention
 menu_category = "utils"
 
 
-@legend.legend_cmd(
-    pattern="legend$",
-    command=("legend", menu_category),
+@guardian.guardian_cmd(
+    pattern="guardian$",
+    command=("guardian", menu_category),
     info={
         "header": "To check bot's alive status",
         "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
         "usage": [
-            "{tr}legend",
+            "{tr} guardian",
         ],
     },
 )
@@ -41,7 +41,7 @@ async def amireallyalive(event):
     reply_to_id = await reply_id(event)
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    legendevent = await eor(event, "`Checking...`")
+    guardianevent = await eor(event, "`Checking...`")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
@@ -50,7 +50,7 @@ async def amireallyalive(event):
     lal = list(EMOJI.split())
     EMOTES = random.choice(lal)
     sweetie_caption = (
-        "**⚜ LegendBot Is Online ⚜**\n\n" + f"{gvarstatus('ALIVE_TEMPLATE')}"
+        "**⚜ GuardianBot Is Online ⚜**\n\n" + f"{gvarstatus('ALIVE_TEMPLATE')}"
     )
     caption = sweetie_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
@@ -58,7 +58,7 @@ async def amireallyalive(event):
         mention=mention,
         uptime=uptime,
         telever=version.__version__,
-        legendver=legendversion,
+        guardianver=guardianversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
@@ -69,8 +69,8 @@ async def amireallyalive(event):
         await event.delete()
     except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
         return await eor(
-            legendevent,
-            f"**Media Value Error!!**\n__Change the link by __`.setdv`\n\n**__Can't get media from this link :-**__ `{LEGEND_IMG}`",
+            guardianevent,
+            f"**Media Value Error!!**\n__Change the link by __`.setdv`\n\n**__Can't get media from this link :-**__ `{GUARDIAN_IMG}`",
         )
 
 
@@ -79,13 +79,13 @@ temp = {ALIVE_TEXT}
 **{EMOTES} Master:** {mention}
 **{EMOTES} Uptime :** `{uptime}`
 **{EMOTES} Telethon Version :** `{telever}`
-**{EMOTES} Legenduserbot Version :** `{legendver}`
+**{EMOTES} Guardianuserbot Version :** `{guardianver}`
 **{EMOTES} Python Version :** `{pyver}`
 **{EMOTES} Database :** `{dbhealth}`
 """
 
 
-@legend.legend_cmd(
+@guardian.guardian_cmd(
     pattern="alive$",
     command=("alive", menu_category),
     info={
@@ -103,13 +103,13 @@ async def amireallyalive(event):
     a = gvarstatus("ALIVE_EMOJI") or "✥"
     kiss = list(a.split())
     EMOJI = random.choice(kiss)
-    legend_caption = "**LegendBot Is Online**\n\n"
-    legend_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-    legend_caption += f"**{EMOJI} Legenduserbot Version :** `{legendversion}`\n"
-    legend_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-    legend_caption += f"**{EMOJI} Uptime :** {uptime}\n"
-    legend_caption += f"**{EMOJI} Master:** {mention}\n"
-    results = await event.client.inline_query(Config.BOT_USERNAME, legend_caption)
+    guardian_caption = "**GuardianBot Is Online**\n\n"
+    guardian_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
+    guardian_caption += f"**{EMOJI} Guardianuserbot Version :** `{guardianversion}`\n"
+    guardian_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
+    guardian_caption += f"**{EMOJI} Uptime :** {uptime}\n"
+    guardian_caption += f"**{EMOJI} Master:** {mention}\n"
+    results = await event.client.inline_query(Config.BOT_USERNAME, guardian_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
 
